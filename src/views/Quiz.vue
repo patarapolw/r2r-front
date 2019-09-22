2afbfd6c2b31bd4ab6828187e65eceb6f9ecf40e
patarapolw <patarapolw@gmail.com>
2019-09-22 14:15:02 +0700

    update

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -9,14 +9,24 @@ import { fetchJSON } from "../util";
 
 @Component
 export default class Quiz extends Vue {
-  public items: any[] = [];
+  public items: any[] = [
+    {name: "HSK"},
+    {children: [
+      {name: "HSK1", stat: {new: 518, due: 0, leech: 0}},
+      {name: "HSK2", stat: {new: 507, due: 0, leech: 0}},
+      {name: "HSK3", stat: {new: 1053, due: 4, leech: 8}},
+      {name: "HSK4", stat: {new: 1832, due: 239, leech: 279}},
+      {name: "HSK5", stat: {new: 4646, due: 0, leech: 0}},
+      {name: "HSK6", stat: {new: 9009, due: 0, leech: 0}}
+    ]}
+  ];
   public tree: any[] = [];
   public q: string = "";
 
   private isLoading = false;
 
   public mounted() {
-    this.getTreeviewData();
+    // this.getTreeviewData();
   }
 
   private async getTreeviewData() {
